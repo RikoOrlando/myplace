5f77ed7e85c4f19925bdd6aab9e5b2ee318ae302
Riko Orlando <rikoorlando76@gmail.com>
2019-12-17 22:52:44 -0500

    seed user

--- seeders/20191218031603-user.js
+++ seeders/20191218031603-user.js
@@ -1,55 +1,55 @@
 'use strict';
 
 let data = [{
-  first_name: 'Febriani',
-  last_name: 'Kenedy',
-  username: 'febrianikenedy',
-  email: 'febrianikenedy@gmail.com',
-  password: 'febrianikenedy',
+  first_name: 'Riko',
+  last_name: 'Orlando',
+  username: 'rikoorlando',
+  email: 'rikoorlando@gmail.com',
+  password: 'rikoorlando',
   description: 'Jangan Menyerah',
   birthday:null,
   createdAt:new Date(),
   updatedAt:new Date()
 },
 {
-  first_name: 'Febriani',
-  last_name: 'Kenedy',
-  username: 'febrianikenedy',
-  email: 'febrianikenedy@gmail.com',
-  password: 'febrianikenedy',
+  first_name: 'Sera',
+  last_name: 'Fim',
+  username: 'serafim',
+  email: 'serafim@gmail.com',
+  password: 'serafim',
   description: 'Jangan Menyerah',
   birthday:null,
   createdAt:new Date(),
   updatedAt:new Date()
 },
 {
-  first_name: 'Febriani',
-  last_name: 'Kenedy',
-  username: 'febrianikenedy',
-  email: 'febrianikenedy@gmail.com',
-  password: 'febrianikenedy',
+  first_name: 'Jimmy',
+  last_name: 'Leonardo',
+  username: 'jimmyleonardo',
+  email: 'jimmyleonardo@gmail.com',
+  password: 'jimmyleonardo',
   description: 'Jangan Menyerah',
   birthday:null,
   createdAt:new Date(),
   updatedAt:new Date()
 },
 {
-  first_name: 'Febriani',
-  last_name: 'Kenedy',
-  username: 'febrianikenedy',
-  email: 'febrianikenedy@gmail.com',
-  password: 'febrianikenedy',
+  first_name: 'Bayu',
+  last_name: 'Octari',
+  username: 'bayuoctari',
+  email: 'bayuoctari@gmail.com',
+  password: 'bayuoctari',
   description: 'Jangan Menyerah',
   birthday:null,
   createdAt:new Date(),
   updatedAt:new Date()
 },
 {
-  first_name: 'Febriani',
-  last_name: 'Kenedy',
-  username: 'febrianikenedy',
-  email: 'febrianikenedy@gmail.com',
-  password: 'febrianikenedy',
+  first_name: 'Novi',
+  last_name: 'Andriani',
+  username: 'noviandriani',
+  email: 'noviandriani@gmail.com',
+  password: 'noviandriani',
   description: 'Jangan Menyerah',
   birthday:null,
   createdAt:new Date(),
@@ -72,6 +72,8 @@ let data = [{
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
+    
+    return queryInterface.bulkInsert('Users', data, {});
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
@@ -85,6 +87,7 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Users', null, {});
     /*
       Add reverting commands here.
       Return a promise to correctly handle asynchronicity.
