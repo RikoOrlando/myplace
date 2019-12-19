ae4af08cacaf896fd4d118ae9265ed7ac7c5c82f
Riko Orlando <rikoorlando76@gmail.com>
2019-12-19 07:29:04 -0500

    tmbah delete

--- models/userplace.js
+++ models/userplace.js
@@ -3,6 +3,12 @@ module.exports = (sequelize, DataTypes) => {
   const Model = sequelize.Sequelize.Model
   class UserPlace extends Model{}
   UserPlace.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
     UserId: DataTypes.INTEGER,
     PlaceId: DataTypes.INTEGER,
     rating: DataTypes.INTEGER,
